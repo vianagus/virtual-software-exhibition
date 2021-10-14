b80555199545cd13240c9b64cb563ff7439d7ca3
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-10-14 12:54:11 +0700

    user camera look

---
+++ Assets/Scripts/UserController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class UserController : MonoBehaviour
+{
+    [SerializeField] Camera userCamera;
+    [SerializeField] Transform userCameraPosition;
+    [SerializeField] float userCameraSpeed;
+    private float xRotation;
+    private float yRotation;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        userCamera.transform.position = userCameraPosition.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            CameraLook();
+        }
+    }
+
+    private void CameraLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // vertical look
+        yRotation -= mouseX;
+
+        // horizontal look
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        userCamera.transform.localEulerAngles = new Vector3(xRotation, yRotation, 0) * userCameraSpeed;
+    }
+}
