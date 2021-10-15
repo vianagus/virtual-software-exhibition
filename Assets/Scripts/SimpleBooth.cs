fac27b1105b56aafbc15cb00dfb3acc7f385f34e
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-10-15 10:59:54 +0700

    simple booth

---
+++ Assets/Scripts/SimpleBooth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SimpleBooth : MonoBehaviour
+{
+    [SerializeField] Sprite poster;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.drawMode = SpriteDrawMode.Sliced;
+        spriteRenderer.size = new Vector2(0.8411f, 1.189f); // A0 paper size
+        spriteRenderer.sprite = poster;
+    }
+}
