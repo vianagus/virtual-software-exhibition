7b77b57eaebaa7d3cddfa852351126baab96a4b2
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-10-18 09:00:54 +0700

    create UI navigation menu

---
+++ Assets/Scripts/UINavigationMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class UINavigationMenu : MonoBehaviour
+{
+    [Header("Initialize Buttons")]
+    [SerializeField] GameObject openNavButton;
+    [SerializeField] GameObject contentButtons;
+
+    private void Start()
+    {
+        contentButtons.SetActive(false);
+    }
+
+    // open navigation menu by button click
+    public void OnOpenNavButtonClick()
+    {
+        openNavButton.SetActive(false);
+        contentButtons.SetActive(true);
+    }
+
+    // close navigation menu by button click
+    public void OnCloseNavButtonClick()
+    {
+        openNavButton.SetActive(true);
+        contentButtons.SetActive(false);
+    }
+}
