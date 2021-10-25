7170691d42ea58511e30bcf3ac475aff0be48abf
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-10-25 14:25:45 +0700

    apply 3D audio to big screen

--- Assets/Scripts/BigScreen.cs
+++ Assets/Scripts/BigScreen.cs
@@ -4,33 +4,17 @@ using UnityEngine.Video;
 public class BigScreen : MonoBehaviour
 {
     [SerializeField] VideoClip videoClip;
-    [Range(0,1)] [SerializeField] float volume;
-    [SerializeField] float audioRange;
-    private VideoPlayer videoPlayer;
 
-    private UserController user;
-    private float distanceToUser;
+    private AudioSource audioSource;
+    private VideoPlayer videoPlayer;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         videoPlayer = GetComponentInChildren<VideoPlayer>();
         videoPlayer.aspectRatio = VideoAspectRatio.FitOutside;
         videoPlayer.clip = videoClip;
         videoPlayer.isLooping = true;
-
-        user = FindObjectOfType<UserController>();
     }
 
-    private void Update() 
-    {
-        distanceToUser = Vector3.Distance(transform.position, user.transform.position);
-        if(distanceToUser <= audioRange)
-        {
-            videoPlayer.SetDirectAudioVolume(0, volume);
-        }
-        else
-        {
-            videoPlayer.SetDirectAudioVolume(0, 0);
-        }
-    }
 }
