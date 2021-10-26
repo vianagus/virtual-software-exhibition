c5ad1786453cc3f2489ca340f9b8ae6d6ac5356a
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-10-26 13:31:32 +0700

    advance stand booth prototype

--- Assets/Scripts/BigScreen.cs
+++ Assets/Scripts/BigScreen.cs
@@ -4,13 +4,10 @@ using UnityEngine.Video;
 public class BigScreen : MonoBehaviour
 {
     [SerializeField] VideoClip videoClip;
-
-    private AudioSource audioSource;
     private VideoPlayer videoPlayer;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         videoPlayer = GetComponentInChildren<VideoPlayer>();
         videoPlayer.aspectRatio = VideoAspectRatio.FitOutside;
         videoPlayer.clip = videoClip;
---
+++ Assets/Scripts/StandBoothPrototype.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(StandBooth))]
+public class StandBoothPrototype : MonoBehaviour
+{
+    private StandBooth standBooth;
+    private SpriteRenderer poster;
+    private VideoPlayer video;
+    
+
+    private void Awake()
+    {
+        standBooth = GetComponent<StandBooth>();
+        poster = GetComponentInChildren<SpriteRenderer>();
+        video = GetComponentInChildren<VideoPlayer>();
+
+        SetVideo(standBooth.GetVideo());
+    }
+
+    private void Update()
+    {
+        SetPoster(standBooth.GetPoster());
+    }
+
+    private void SetPoster(Sprite posterSprite)
+    {
+        poster.drawMode = SpriteDrawMode.Sliced;
+        poster.size = new Vector2(0.8411f * 2, 1.189f * 2); // A0 paper size x 2
+        poster.sprite = posterSprite;
+    }
+
+    private void SetVideo(VideoClip videoClip)
+    {
+        video.aspectRatio = VideoAspectRatio.FitOutside;
+        video.clip = videoClip;
+        video.isLooping = true;
+    }
+}
