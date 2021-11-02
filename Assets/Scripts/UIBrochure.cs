cf9fc3b0443270c8516b2daa09bf517d59d16295
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-11-02 09:59:45 +0700

    UI Brochure: add logo, product name, product description

--- Assets/Scripts/UIBrochure.cs
+++ Assets/Scripts/UIBrochure.cs
@@ -4,18 +4,12 @@ using UnityEngine.UI;
 
 public class UIBrochure : MonoBehaviour
 {
-    [Header("Container")]
+    [Header("Initialize Container")]
     [SerializeField] GameObject brochureContainer;
-    [SerializeField] GameObject identityContainer;
-    [SerializeField] GameObject pitchContainer;
-    [SerializeField] GameObject descriptionContainer;
 
-    [Header("Identity")]
-    [SerializeField] Image logo;
+    [Header("Initialize Contents")]
+    [SerializeField] Image productLogo;
     [SerializeField] Text productName;
-
-    [Header("Pitch & Description")]
-    [SerializeField] Text pitch;
     [SerializeField] Text description;
 
     private void Start() 
@@ -27,12 +21,7 @@ public class UIBrochure : MonoBehaviour
     {
         this.productName.text = productName;
         if(logo == null) return;
-        this.logo.sprite = logo;
-    }
-
-    private void SetPitch(string pitch)
-    {
-        this.pitch.text = pitch;
+        this.productLogo.sprite = logo;
     }
 
     private void SetDescription(string description)
@@ -43,34 +32,14 @@ public class UIBrochure : MonoBehaviour
     public void OpenBrochure(StandBooth standBooth)
     {
         SetIdentity(standBooth.GetLogo(), standBooth.GetProductName());
-        SetPitch(standBooth.GetProductPitch());
         SetDescription(standBooth.GetProductDescription());
 
         brochureContainer.SetActive(true);
-
-        //StartCoroutine(ShowContent(1));
     }
 
     public void CloseBrochure()
     {
         //HideContent();
         brochureContainer.SetActive(false);
-    }
-
-    private IEnumerator ShowContent(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        identityContainer.SetActive(true);
-        yield return new WaitForSeconds(duration);
-        pitchContainer.SetActive(true);
-        yield return new WaitForSeconds(duration);
-        descriptionContainer.SetActive(true);
-    }
-
-    private void HideContent()
-    {
-        identityContainer.SetActive(false);
-        pitchContainer.SetActive(false);
-        descriptionContainer.SetActive(false);
     }
 }
