ed5012514a18a44f612e7f34297d6e8c334a96c9
Agus Julpian Alwi <agus.julpian@gmail.com>
2021-11-15 11:44:03 +0700

    close UIs with button

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject posterContainer;
+    [SerializeField] private GameObject brochureContainer;
+
+    [Header("Navigation Menu")]
+    [SerializeField] private GameObject navMenuButton;
+    [SerializeField] private GameObject navMenu;
+    [SerializeField] private GameObject navMenuStandBoothList;
+    [SerializeField] private GameObject navMenuHelp;
+
+    private void Update()
+    {
+        // close UIs system
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            if(navMenu.activeInHierarchy)
+            {
+                if(navMenuStandBoothList.activeInHierarchy || navMenuHelp.activeInHierarchy)
+                {
+                    navMenuStandBoothList.SetActive(false);
+                    navMenuHelp.SetActive(false);
+                    return;
+                }
+
+                navMenuButton.SetActive(true);
+                navMenu.SetActive(false);
+                return;
+            }
+
+            if(posterContainer.activeInHierarchy) { posterContainer.SetActive(false); return; }
+            if(brochureContainer.activeInHierarchy) { brochureContainer.SetActive(false); return; }
+        }
+    }
+}
